b52c8809aacf740c1ca6a99a2da54351fcbf5506
Morgan Hansen <10598582@uvu.edu>
2015-09-02 15:05:18 -0600

    C#1

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	int newInt =8;
+	void Start () {
+		newInt = DividebyTwo (newInt);
+		Debug.Log (newInt);
+	}
+	int DividebyTwo (int number){
+
+	int ret;
+
+		ret = number / 2;
+
+		return ret;
+	
+	
+	}
+}
