899b68caeb72b02cc09dc1bc9d65d7839889e521
Morgan Hansen <10598582@uvu.edu>
2015-09-28 15:28:30 -0600

    do do while for for each

---
+++ NewUnityProject/Assets/ForEachLoop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForEachLoop : MonoBehaviour 
+{   
+	void Start () 
+	{
+		string[] strings = new string[3];
+
+		strings [0] = "first string";
+		strings [1] = "second string";
+		strings [2] = "third string";
+
+		foreach (string item in strings)
+		{
+		print (item);
+		}
+	}
+}
